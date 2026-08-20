417607297014fb6b3bb69ff2bdbe2fa7d4b63fa0
harrodswill <W.A.Stafford@sms.ed.ac.uk>
2026-08-20 18:34:41 +0100

    Comitting Test with Example

---
+++ Billboard Songs Example.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+billboard_longer <- billboard |> 
+  pivot_longer(
+    cols = starts_with("wk"), 
+    names_to = "week", 
+    values_to = "rank",
+    values_drop_na = TRUE
+  ) |> 
+  mutate(
+    week = parse_number(week)
+  )
+
+billboard_longer |> 
+  ggplot(aes(x = week, y = rank, group = track)) + 
+  geom_line(alpha = 0.25) + 
+  scale_y_reverse()
